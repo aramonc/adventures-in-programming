2526270e31f5215df0e0ddfed98f840bddf0b5ca
Adrian Cardenas <arcardenas@gmail.com>
2017-09-03 13:00:58 +0200

    Fix Landscape's ridiculous version of fancy box

--- themes/landscape/scripts/fancybox.js
+++ themes/landscape/scripts/fancybox.js
@@ -1,4 +1,4 @@
-var rUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/;
+var rUrl = /[^\s]+/;
 
 /**
 * Fancybox tag
@@ -18,7 +18,7 @@ hexo.extend.tag.register('fancybox', function(args){
   var title = args.join(' ');
 
   return '<a class="fancybox" href="' + original + '" title="' + title + '">' +
-    '<img src="' + (thumbnail || original) + '" alt="' + title + '">'
+    '<img src="' + (thumbnail || original) + '" alt="' + title + '">' +
     '</a>' +
     (title ? '<span class="caption">' + title + '</span>' : '');
 });
